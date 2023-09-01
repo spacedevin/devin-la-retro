88d568f1fdc9a043c435fb8f4b18efce49c2b91d
space <git@arzynik.com>
2023-09-01 15:39:35 -0700

    add redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/resume',
+        destination: 'https://www.figma.com/file/5Ov2RzdipyCZOvBjwRHsbI/Space-Resume-2023?type=design&node-id=0%3A1&mode=design&t=aMICpHn06pw9fTPa-1',
+        permanent: false,
+      },
+    ]
+  },
+}
